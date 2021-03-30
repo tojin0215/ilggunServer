843cf44af6a0e89437e87c5cc9bdafd742a54cf3
Debian <bitnami@ip-172-26-4-243.ap-northeast-2.compute.internal>
2021-03-30 21:17:30 +0900

    image upload

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ console.log(JSON.parse(req.body.body).file);
 	res.send('success');
 }
 app.use(express.static('public'));
-app.use(bodyParser.json({limit:"50mb"}));
-app.use(bodyParser.urlencoded({limit:"50mb", extended:true, parameterLimit:500000000}));
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({limit:"50mb", extended:true, parameterLimit:500000000}));
 app.use(cookieParser());
 app.use(cors({origin: true, credentials: true}));
 // configuration =========================
