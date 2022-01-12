090291c2be788a286de36f8b9586b48b479bafbb
xcv <xcv4716@naver.com>
2022-01-12 15:42:57 +0900

    loginHistroy

--- api/index.js
+++ api/index.js
@@ -7,18 +7,20 @@ const exercisepack = require('./exercisepack');
 const manager = require('./manager');
 const inbody = require('./inbody');
 const customerenter = require('./customerenter');
+const loginHistroy = require('./loginHistroy');
 
 //api 파일 생성할 때 마다 index에도 함께 추가
 const routers = {
-    customer:customer,
-    sales:sales,
+    loginHistroy: loginHistroy,
+    customer: customer,
+    sales: sales,
     customerenter: customerenter,
-    exercise:exercise,
-    assignexercise:assignexercise,
-    exerciselink:exerciselink,
-    exercisepack:exercisepack,
-    manager:manager,
-    inbody:inbody,
+    exercise: exercise,
+    assignexercise: assignexercise,
+    exerciselink: exerciselink,
+    exercisepack: exercisepack,
+    manager: manager,
+    inbody: inbody,
 }
 
 module.exports = routers;
