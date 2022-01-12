d6124ec1d2bbcbed62c6a0e10e2158b62fe99b01
xcv <xcv4716@naver.com>
2022-01-12 16:02:18 +0900

    string으로 변경

--- model/loginHistory.js
+++ model/loginHistory.js
@@ -1,7 +1,7 @@
 export default (sequelize, DataTypes) => {
   return sequelize.define('loginHistory', {
     bang: { type: DataTypes.STRING(50) },
-    login_time: { type: DataTypes.INTEGER },
+    login_time: { type: DataTypes.STRING(100) },
     numberofworkers: { type: DataTypes.INTEGER }
 
   }, {
